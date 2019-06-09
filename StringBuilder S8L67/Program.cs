b3387115a7063f2d4b2b01a0841a25c6bdc7275f
Sartius <amer_hrnjic@hotmail.com>
2019-06-09 14:25:39 +0200

    Import Project

---
+++ StringBuilder S8L67/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder_S8L67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10); // can all be appended
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine(builder[0]);
+
+            var name = "Mosh";
+        }
+    }
+}
